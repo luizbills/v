70b14cf9671bae2f39c00db172999c280092848a
Luiz Paulo "Bills <luizbills@pm.me>
2019-09-08 12:44:57 -0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -7,15 +7,19 @@ function v ( $value, ...$filters ) {
 }
 
 function v_register_filter ( $name, $callback, $context = null ) {
-	return Engine::get_instance()->register_filter( $name, $callback, $context );
+	Engine::get_instance()->register_filter( $name, $callback, $context );
 }
 
 function v_set_context ( $context ) {
-	return Engine::get_instance()->set_context( $context );
+	Engine::get_instance()->set_context( $context );
 }
 
 function v_reset_context () {
-	return Engine::get_instance()->reset_context();
+	Engine::get_instance()->reset_context();
+}
+
+function v_load_default_filters ( $extension ) {
+	Engine::get_instance()->load( $extension );
 }
 
 // private helper
