36eebdfd8511d9071efa601cdc72dd4947a38a14
Luiz Bills <luizpbills@gmail.com>
2019-09-10 12:44:01 -0300

    add arguments type

--- src/functions.php
+++ src/functions.php
@@ -3,14 +3,14 @@
 use luizbills\v\Engine;
 
 function v ( $value, ...$filters ) {
-	return Engine::get_instance()->run_filters( $value, $filters );
+	return Engine::get_instance()->run_filters( $value, ...$filters );
 }
 
-function v_register_filter ( $name, $callback, $context = null ) {
+function v_register_filter ( string $name, callable $callback, string $context = '' ) {
 	Engine::get_instance()->register_filter( $name, $callback, $context );
 }
 
-function v_set_context ( $context ) {
+function v_set_context ( string $context ) {
 	Engine::get_instance()->set_context( $context );
 }
 
@@ -18,6 +18,6 @@ function v_reset_context () {
 	Engine::get_instance()->reset_context();
 }
 
-function v_load_default_filters ( $extension ) {
+function v_load_default_filters ( array $extension ) {
 	Engine::get_instance()->load( $extension );
 }
