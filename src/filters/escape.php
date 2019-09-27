2e1d7ade6734472ad0631cc02a4cd514b8826d63
Luiz Paulo "Bills <luizbills@pm.me>
2019-09-27 13:29:49 -0300

    Update escape.php

--- src/filters/escape.php
+++ src/filters/escape.php
@@ -2,5 +2,5 @@
 
 // Escape $value
 return function ( $value, $args ) {
-	return \htmlspecialchars( (string) $value, \ENT_QUOTES );
+	return \htmlspecialchars( (string) $value, \ENT_QUOTES, 'UTF-8' );
 };
