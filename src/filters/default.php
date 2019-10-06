3598afa830186f8f0f1b0d4e81ac0c23c6119e36
Atthaphon Urairat <aurairat@inetasia.com>
2019-10-06 18:20:05 +0700

    Improve `default` filter

--- src/filters/default.php
+++ src/filters/default.php
@@ -1,10 +1,10 @@
 <?php
 
 // Return a default value if empty string is passed
-return function ( $value, $args ) {
-	$default = (string) $args->get( 0 );
-	if ( '' == $default ) {
-		throw new \InvalidArgumentException( 'Argument 1 should not be a empty string' );
-	}
-	return '' !== $value ? $value : $default;
+return function ($value, $args) {
+    $default = (string) $args->get(0);
+    if (empty($default) || $default == "''") {
+        throw new \InvalidArgumentException('Argument 1 should not be a empty string');
+    }
+    return !empty($value) ? $value : $default;
 };
