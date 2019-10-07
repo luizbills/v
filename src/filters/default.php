282647eb09e6f8b90eac7f18f1855bae4146faec
Luiz Paulo "Bills <luizbills@pm.me>
2019-10-07 09:21:01 -0300

    fix null comparison

--- src/filters/default.php
+++ src/filters/default.php
@@ -3,7 +3,7 @@
 // Return a default value if empty string is passed
 return function ($value, $args) {
     $default = (string) $args->get(0);
-    if ( empty( $default ) || $default == '' ) {
+    if ( null === $default || '' == $default ) {
 		throw new \InvalidArgumentException( 'Argument 1 should not be a empty string or null' );
     }
     return ! empty( $value ) ? $value : $default;
