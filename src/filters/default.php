5996fa0075b14973bf11e71f11901fde1f936320
Luiz Paulo "Bills <luizbills@pm.me>
2019-10-07 09:20:01 -0300

    fix empty string comparison and fix code style

--- src/filters/default.php
+++ src/filters/default.php
@@ -3,8 +3,8 @@
 // Return a default value if empty string is passed
 return function ($value, $args) {
     $default = (string) $args->get(0);
-    if (empty($default) || $default == "''") {
-        throw new \InvalidArgumentException('Argument 1 should not be a empty string');
+    if ( empty( $default ) || $default == '' ) {
+		throw new \InvalidArgumentException( 'Argument 1 should not be a empty string or null' );
     }
-    return !empty($value) ? $value : $default;
+    return ! empty( $value ) ? $value : $default;
 };
