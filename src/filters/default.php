fd7659e8148589ae7cf0d22f8e0d3e5fe0390f7f
Atthaphon Urairat <aurairat@inetasia.com>
2019-10-08 07:53:01 +0700

    Deleted null and updated style to match coding standard

--- src/filters/default.php
+++ src/filters/default.php
@@ -1,10 +1,10 @@
 <?php
 
 // Return a default value if empty string is passed
-return function ($value, $args) {
-    $default = (string) $args->get(0);
-    if (null !== $default && '' == $default) {
-        throw new \InvalidArgumentException('Argument 1 should not be a empty string');
-    }
-    return null !== $value && '' !== $value ? $value : $default;
+return function ( $value, $args ) {
+	$default = (string) $args->get( 0 );
+	if ( '' == $default ) {
+		throw new \InvalidArgumentException( 'Argument 1 should not be a empty string' );
+	}
+	return null !== $value && '' !== $value ? $value : $default;
 };
