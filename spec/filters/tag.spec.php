50c44920ba3187567c3948ae8ef084f88b412d19
Luiz Paulo "Bills <luizbills@pm.me>
2019-10-28 13:31:37 -0300

    fix tag tests

--- spec/filters/tag.spec.php
+++ spec/filters/tag.spec.php
@@ -2,7 +2,7 @@
 
 describe( 'filter: tag', function() {
 	it( 'shoulds make the value into an html tag', function() {
-		$current = v( 'My Title', 'tag(h1, "my-id", "my-class")', 'raw' );
+		$current = v( 'My Title', 'tag("h1", "my-class", "my-id")', 'raw' );
 		$expected = '<h1 id="my-id" class="my-class">My Title</h1>';
 		expect( $current )->toBe( $expected );
 	} );
