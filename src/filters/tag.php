ede686adb8eef0d8c6246a7e49a6e4d50144bb07
Luiz Paulo "Bills <luizbills@pm.me>
2019-10-28 13:30:15 -0300

    put class before id and update code style

--- src/filters/tag.php
+++ src/filters/tag.php
@@ -1,14 +1,14 @@
 <?php
 
-return function ($value, $args) {
-    $tag = $args->get(0, null);
-    $id = $args->get(1, null);
-    $class = $args->get(2, null);
+return function ( $value, $args ) {
+    $tag = $args->get( 0, null );
+    $class = $args->get( 1, null );
+	$id = $args->get( 2, null );
 
     return '<'
         . $tag
-        . (!is_null($id) ? ' id="' . $id . '"' : '')
-        . (!is_null($class) ? ' class="' . $class . '"' : '')
+        . ( ! is_null( $id ) ? ' id="' . $id . '"' : '' )
+        . ( ! is_null( $class ) ? ' class="' . $class . '"' : '' )
         . '>'
         . $value
         . '</' .$tag . '>';
