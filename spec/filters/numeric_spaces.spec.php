8fb19f3aef40ac9c15076f38c0d8630791f17fcb
Luiz Paulo "Bills <luizbills@pm.me>
2019-12-11 13:04:05 +0000

    fix typo

--- spec/filters/numeric_spaces.spec.php
+++ spec/filters/numeric_spaces.spec.php
@@ -1,7 +1,7 @@
 <?php
 
 describe( 'filter: numeric_spaces', function() {
-	it( 'shoulds remove all non-numeric chars, but leave white spaces', function() {
+	it( 'shoulds remove all non-numeric chars but leaves white spaces', function() {
 		$current = v( '12#3. 456a', 'numeric_spaces' );
 		$expected = '123 456';
 		expect( $current )->toBe( $expected );
