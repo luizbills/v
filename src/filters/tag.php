b5d8f45b32dcc7a221ea9e01f20f823db5e9839a
Luiz Paulo "Bills <luizbills@pm.me>
2019-12-11 13:38:57 +0000

    improve 'tag' filter

--- src/filters/tag.php
+++ src/filters/tag.php
@@ -1,15 +1,13 @@
 <?php
 
 return function ( $value, $args ) {
-	$tag = $args->get( 0, null );
-	$class = $args->get( 1, null );
-	$id = $args->get( 2, null );
+	$tag = (string) $args->get( 0 );
+	$class = $args->get( 1, '' );
+	$id = $args->get( 2, '' );
 
-	return '<'
-		. $tag
-		. ( ! is_null( $id ) ? ' id="' . $id . '"' : '' )
-		. ( ! is_null( $class ) ? ' class="' . $class . '"' : '' )
-		. '>'
-		. $value
-		. '</' .$tag . '>';
+	if ( '' == $tag ) {
+		throw new InvalidArgumentException( 'First argument of "tag" filter must be a non-empty string' );
+	}
+
+	return "<$tag id='$id' class='$class'>$value</$tag>";
 };
