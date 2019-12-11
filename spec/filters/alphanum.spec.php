f44ff2c3446b46eb745586a261512282f397dbc7
Luiz Paulo "Bills <luizbills@pm.me>
2019-12-11 13:01:15 +0000

    fix 'alphanum' test

--- spec/filters/alphanum.spec.php
+++ spec/filters/alphanum.spec.php
@@ -1,6 +1,6 @@
 <?php
 
-describe( 'filter: alpha', function() {
+describe( 'filter: alphanum', function() {
 	it( 'shoulds remove all non-alphabetic and non-numeric characters', function() {
 		$current = v( 'A bc)d$8', 'alphanum' );
 		$expected = 'Abcd8';
