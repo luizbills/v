92d42d58d60b362d6d9c80a672fec665139141ee
Luiz Paulo "Bills <luizbills@pm.me>
2019-12-11 13:55:40 +0000

    sanitize 'tag' filter inputs

--- src/filters/tag.php
+++ src/filters/tag.php
@@ -9,5 +9,9 @@ return function ( $value, $args ) {
 		throw new InvalidArgumentException( 'First argument of "tag" filter must be a non-empty string' );
 	}
 
+	$tag = filter_var( $tag, FILTER_SANITIZE_STRING );
+	$id = filter_var( $id, FILTER_SANITIZE_STRING );
+	$class = filter_var( $class, FILTER_SANITIZE_STRING );
+
 	return "<$tag id='$id' class='$class'>$value</$tag>";
 };
