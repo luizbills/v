0657ebcfb2134dec6a6e78bd4934f05d365ee244
Luiz "Bills <luizbills@pm.me>
2020-01-09 19:11:12 +0000

    improve safe_html filter

--- src/filters/safe_html.php
+++ src/filters/safe_html.php
@@ -2,11 +2,10 @@
 
 // Remove <script> and <style> tags
 return function ( $value, $args ) {
-	// remove iframe and link tags
-	$value = \preg_replace( '/<(iframe|link)[^>]*?>/si', '', (string) $value );
-	// remove script and style tags
-	$value = \preg_replace( '/<(script|style)[^>]*?>.*?<\/\\1>/si', '', (string) $value );
-	$value = \preg_replace( '/<(script|style)[^>]*?>/si', '', (string) $value );
+	// remove all script and style tags with code
+	$value = \preg_replace( '/<(script|style)[^>]*?>.*?<\/\\1>/si', '', $value );
+	// remove any script, style, link and iframe tags
+	$value = \preg_replace( '/<(script|style|iframe|link)[^>]*?>/si', '', $value );
 
 	return $value;
 };
