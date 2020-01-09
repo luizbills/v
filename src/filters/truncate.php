00e237dfc0721efd902897f12c5c92e0e5e10cc4
Luiz "Bills <luizbills@pm.me>
2020-01-09 19:11:22 +0000

    improve truncate filter

--- src/filters/truncate.php
+++ src/filters/truncate.php
@@ -3,5 +3,8 @@
 return function ( $value, $args ) {
 	$size = $args->get( 0, 1 );
 	$enconding = $args->get( 1, mb_internal_encoding() );
-	return \mb_substr( $value, 0, $size, $enconding );
+	if ( mb_strlen( $value, $enconding ) > $size ) {
+		return \mb_substr( $value, 0, $size, $enconding );
+	}
+	return $value;
 };
