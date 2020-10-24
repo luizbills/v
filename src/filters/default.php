17889000eff20425a29b38ba37cd505e57e3641f
Luiz "Bills <luizbills@pm.me>
2020-10-24 13:32:53 +0000

    update default filter

--- src/filters/default.php
+++ src/filters/default.php
@@ -1,10 +1,10 @@
 <?php
 
-// Return a default value if empty string is passed
+// Return a default value if a falsy value is passed
 return function ( $value, $args ) {
 	$default = (string) $args->get( 0 );
 	if ( '' == $default ) {
 		throw new \InvalidArgumentException( 'Argument 1 should not be a empty string' );
 	}
-	return null !== $value && '' !== $value ? $value : $default;
+	return ! empty( $value ) ? $value : $default;
 };
