e29040b872920222163630ba80dd13fb20f51964
Luiz "Bills <luizbills@pm.me>
2020-12-26 15:08:03 -0300

    Update tag.spec.php

--- spec/filters/tag.spec.php
+++ spec/filters/tag.spec.php
@@ -2,8 +2,8 @@
 
 describe( 'filter: tag', function() {
 	it( 'shoulds make the value into an html tag', function() {
-		$current = v( 'My Title', 'tag("h1", "my-class", "my-id")', 'raw' );
-		$expected = "<h1 id='my-id' class='my-class'>My Title</h1>";
+		$current = v( 'My Title', 'tag("h1", "my-class1 my-class2", "my-id")', 'raw' );
+		$expected = "<h1 class='my-class1 my-class2' id='my-id'>My Title</h1>";
 		expect( $current )->toBe( $expected );
 	} );
 } );
