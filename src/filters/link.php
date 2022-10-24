8e5f41968f3b9e0df143902e3c24eb1e0aa67066
marcoafp <115903635+marcoafp@users.noreply.github.com>
2022-10-24 15:37:45 -0300

    improve link filter

--- src/filters/link.php
+++ src/filters/link.php
@@ -3,9 +3,20 @@
 return function ( $value, $args ) {
 	$url = $args->get( 0, '#' );
 	$class = $args->get( 1, '' );
+	$target = $args->get( 2, '' );
+	$follow = $args->get( 3, '' );
 
 	$url = filter_var( $url, FILTER_SANITIZE_URL );
 	$class = filter_var( $class, FILTER_SANITIZE_STRING );
+	$target = '_blank' === $target ? $target : filter_var( $target, FILTER_SANITIZE_URL );
 
-	return "<a href='$url' class='$class'>$value</a>";
+	$html = "<a href=\"$url\" class=\"$class\"";
+	if ( $target ) {
+		$html .= " target=\"$target\"";
+		if ( $follow !== 'follow' ) {
+			$html .= ' rel="nofollow noopener"';
+		}
+	}
+
+	return $html . ">$value</a>";
 };
