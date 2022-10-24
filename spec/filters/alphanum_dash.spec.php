ce47fe30549d10fe922f2a15862e3daf764019ea
marcoafp <115903635+marcoafp@users.noreply.github.com>
2022-10-24 15:03:07 -0300

    fix test

--- spec/filters/alphanum_dash.spec.php
+++ spec/filters/alphanum_dash.spec.php
@@ -1,8 +1,8 @@
 <?php
 
-describe( 'filter: alphanum', function() {
+describe( 'filter: alphanum_dash', function() {
 	it( 'shoulds remove all non-alphabetic and non-numeric characters, but leaves dashes and underscores', function() {
-		$current = v( '_A bc)d$é-8', 'alphanum' );
+		$current = v( '_A bc)d$é-8', 'alphanum_dash' );
 		$expected = '_Abcdé-8';
 		expect( $current )->toBe( $expected );
 	} );
